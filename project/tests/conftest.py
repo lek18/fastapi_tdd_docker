958b2115505220add37a8d4d39b2fa40a88b5dd1
Luis Ek <52711401+lek18@users.noreply.github.com>
2023-03-31 15:24:47 -0500

    working tests for summaries with and without db pytest fixtures

--- project/tests/conftest.py
+++ project/tests/conftest.py
@@ -4,6 +4,7 @@ import pytest
 from app.config import Settings, get_settings
 from app.main import create_application
 from starlette.testclient import TestClient
+from tortoise.contrib.fastapi import register_tortoise
 
 
 def get_settings_override():
@@ -20,3 +21,20 @@ def test_app():
         yield test_client
 
     # tear down
+
+
+@pytest.fixture(scope="module")
+def test_app_with_db():
+    # set up
+    app = create_application()
+    app.dependency_overrides[get_settings] = get_settings_override
+    register_tortoise(
+        app,
+        db_url=os.environ.get("DATABASE_TEST_URL"),
+        modules={"models": ["app.models.tortoise"]},
+        generate_schemas=True,
+        add_exception_handlers=True,
+    )
+    with TestClient(app) as test_client:
+        # testing
+        yield test_client
