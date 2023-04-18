48aefc4e66149a016262649e7f1ed829ebbe8460
Luis Ek <52711401+lek18@users.noreply.github.com>
2023-04-17 21:51:22 -0500

    right way to do isort with pre-commit + docker

--- project/app/api/summaries.py
+++ project/app/api/summaries.py
@@ -1,9 +1,10 @@
 from typing import List
 
+from fastapi import APIRouter
+
 from app.api import crud
 from app.models.pydantic import SummaryPayloadSchema, SummaryResponseSchema
 from app.models.tortoise import SummarySchema
-from fastapi import APIRouter
 
 router = APIRouter()
 
