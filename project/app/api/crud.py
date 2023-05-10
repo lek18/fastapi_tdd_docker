9c63407eed6c82aee53b43c357039ae3b56c567a
Luis Ek <52711401+lek18@users.noreply.github.com>
2023-05-10 14:24:00 -0500

    adding background task for the summarize + unit tests

--- project/app/api/crud.py
+++ project/app/api/crud.py
@@ -1,19 +1,13 @@
-import logging
 from typing import List, Union
 
 from fastapi import HTTPException
 
 from app.models.pydantic import SummaryPayloadSchema
 from app.models.tortoise import TextSummary
-from app.summarizer import generate_summary
-
-logging.basicConfig(filename="myapp.log", level=logging.INFO)
 
 
 async def post(payload: SummaryPayloadSchema) -> int:
-    article_summary = generate_summary(payload.url)
-    logging.info(f"here is my article summary\n {article_summary}")
-    summary = TextSummary(url=payload.url, summary=article_summary)
+    summary = TextSummary(url=payload.url, summary="")
     await summary.save()
     return summary.id
 
